79c85f2ea3893bdb2ec4721535239644797b4cd8
Jenny Shamoo <jenny.shamoo@gmail.com>
2020-05-15 14:08:00 -0700

    install redux-thunk & fix redux devtool

--- src/store.js
+++ src/store.js
@@ -1,7 +1,12 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger, thunk))
+);
 
 export default store;
