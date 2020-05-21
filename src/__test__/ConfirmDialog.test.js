8f242f06dc5b5771bb5f2d4c8e0fc5d6f6af697f
Jenny Shamoo <jenny.shamoo@gmail.com>
2020-05-20 17:05:59 -0700

    installed jest, babel-jest, testing-library/react and create the first test file

---
+++ src/__test__/ConfirmDialog.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ConfirmDialog from "../components/ConfirmDialog";
+
+test("loads and displays the modal", () => {
+  expect(1).toBe(1);
+  expect(1 + 1).toBeGreaterThan(1);
+});
