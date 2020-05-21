3efc3a7d8868dda5734c8a97940001fa1149eb36
Jenny Shamoo <jenny.shamoo@gmail.com>
2020-05-20 20:25:53 -0700

    complete test for ConfirmDialog component

--- src/__test__/ConfirmDialog.test.js
+++ src/__test__/ConfirmDialog.test.js
@@ -1,8 +1,29 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import ConfirmDialog from "../components/ConfirmDialog";
 
+test("modal not shown when default state is false", () => {
+  const dog = { name: "Bandit" };
+  render(<ConfirmDialog dog={dog} open={false} />);
+  expect(screen.queryByText("Would you like to adopt")).toBeNull();
+});
+
 test("loads and displays the modal", () => {
-  expect(1).toBe(1);
-  expect(1 + 1).toBeGreaterThan(1);
+  const dog = { name: "Bandit" };
+  render(<ConfirmDialog dog={dog} open={true} />);
+  expect(screen.queryByText("Would you like to adopt Bandit")).toBeDefined();
+});
+
+test("modal closes when yes button is clicked", () => {
+  const dog = { name: "Bandit" };
+  render(<ConfirmDialog dog={dog} open={true} />);
+  fireEvent.click(screen.queryByText(/yes/i));
+  expect(screen.queryByText("Would you like to adopt Bandit")).toBeNull();
+});
+
+test("modal closes when no button is clicked", () => {
+  const dog = { name: "Bandit" };
+  render(<ConfirmDialog dog={dog} open={true} />);
+  fireEvent.click(screen.queryByText(/no/i));
+  expect(screen.queryByText("Would you like to adopt Bandit")).toBeNull();
 });
